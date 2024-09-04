612de5bea182f5e519d1fb3495b1ffa87be6c647
Gustavo Eguchi <gustavoeguchi10@gmail.com>
2024-09-04 16:49:12 -0300

    1061

--- Tempo de um Evento.py
+++ Tempo de um Evento.py
@@ -3,40 +3,52 @@ hora_inicio, texto, minuto_inicio, texto, segundo_inicio = input().split()
 texto, dia_fim = input().split()
 hora_fim, texto, minuto_fim, texto, segundo_fim = input().split()
 
-dias = int(dia_fim) - int(dia_inicio)
-horas = int(hora_fim) - int(hora_inicio)
-if (int(hora_inicio) > int(hora_fim)):
-    horas = (24 - int(hora_inicio)) + int(hora_fim)
-    dias -= 1
+ajuste_minutos = 0
+ajuste_horas = 0
+ajuste_dias = 0
+
+segundos = int(segundo_fim) - int(segundo_inicio)
+if (int(segundo_inicio) > int(segundo_fim)):
+    segundos = (60 - int(segundo_inicio)) + int(segundo_fim)
+    ajuste_minutos -= 1
+elif (int(segundo_inicio) == int(segundo_fim)):
+    segundos = 0
 minutos = int(minuto_fim) - int(minuto_inicio)
 if (int(minuto_inicio) > int(minuto_fim)):
     minutos = (60 - int(minuto_inicio)) + int(minuto_fim)
-    horas -= 1
-segundos = int(segundo_fim) - int(segundo_inicio)
-if (int(segundo_inicio) > int(segundo_fim)):    
-    segundos = (60 - int(segundo_inicio)) + int(segundo_fim)
-    minutos -= 1
+    ajuste_horas -= 1
+elif (int(minuto_inicio) == int(minuto_fim)):
+    if (int(segundo_fim) >= int(segundo_inicio)):
+        minutos = 0
+    else: 
+        minutos = 60
+        ajuste_horas -= 1
+horas = int(hora_fim) - int(hora_inicio)
+if (int(hora_inicio) > int(hora_fim)):
+    horas = (24 - int(hora_inicio)) + int(hora_fim)
+    ajuste_dias -= 1
+elif (int(hora_inicio) == int(hora_fim)):
+    if (int(minuto_fim) >= int(minuto_inicio) and int(segundo_fim) >= int(segundo_inicio)):
+        horas = 0
+    else: 
+        horas = 24
+        if (int(minuto_inicio) >= int(minuto_fim) and int(segundo_inicio) > int(segundo_fim)):
+            ajuste_dias -= 1
+dias = int(dia_fim) - int(dia_inicio)
 
-if (segundos >= 60):
-    segundos -= 60
-    minutos += 1
-elif (segundos < 0):
-    segundos += 1
+minutos += ajuste_minutos
+horas += ajuste_horas
+dias += ajuste_dias
 
-if (minutos >= 60):
-    minutos -= 60
-    horas += 1
-elif (minutos < 0):
-    minutos += 1
-
-if (horas >= 24):
-    horas -= 24
-    dias += 1
-elif (horas < 0):
-    horas += 1
+if (horas == 24):
+    horas = 0
+if (minutos == 60):
+    minutos = 0
 
 print(f'{dias} dia(s)')
 print(f'{horas} hora(s)')
 print(f'{minutos} minuto(s)')
 print(f'{segundos} segundo(s)')
 
+#Não me pede outro exercício com tempo não, por favor
+#Tempo para finalizar +1h:30min 
