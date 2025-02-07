8644047754c0c6392c8517440c6c002fd543479e
Gustavo Eguchi <gustavoeguchi10@gmail.com>
2025-02-07 17:50:18 -0300

    1115

---
+++ Quadrante.py
@@ -0,0 +1,16 @@
+while True:
+    X, Y = input().split()
+    x = int(X)
+    y = int(Y)
+
+    if (x > 0 and y > 0):
+        print('primeiro')
+    elif (x < 0 and y > 0):
+        print('segundo')
+    elif (x < 0 and y < 0):
+        print('terceiro')
+    elif (x > 0 and y < 0):
+        print('quarto')
+    else:
+        break
+
