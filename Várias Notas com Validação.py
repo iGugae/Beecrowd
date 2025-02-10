7692ac054cb4b33e2a485eaabb24461c9c2ea27c
Gustavo Eguchi <gustavoeguchi10@gmail.com>
2025-02-10 17:58:01 -0300

    1118

---
+++ Várias Notas com Validação.py
@@ -0,0 +1,28 @@
+def media():
+    while True:
+        num1 = float(input())
+        if (num1 < 0 or num1 > 10):
+            print('nota invalida')
+        else:
+            break
+
+    while True:
+        num2 = float(input())
+        if (num2 < 0 or num2 > 10):
+            print('nota invalida')
+        else:
+            break
+
+    media = (num1 + num2)/2
+    print(f'media = {media:.2f}')
+
+media()
+while True:
+    print('novo calculo (1-sim 2-nao)')
+    x = int(input())
+    if (x == 1):
+        media()
+    elif (x == 2):
+        break
+    else:
+        continue
