73e548c8a241e096a8db95919ba8a958cc8a9a44
Gustavo Eguchi <gustavoeguchi10@gmail.com>
2025-02-12 19:55:48 -0300

    1146

---
+++ Sequências Crescentes.py
@@ -0,0 +1,11 @@
+while True:
+    num = int(input())
+
+    if (num == 0):
+        break
+    else:
+        for i in range(1,num+1):
+            if (i == num):
+                print(f'{i}')
+            else:
+                print(f'{i}',end=' ')
