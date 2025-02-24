fbeed464afc114da91f401b8fee82f57db8f177a
Gustavo Eguchi <gustavoeguchi10@gmail.com>
2025-02-24 18:44:14 -0300

    1186

---
+++ Abaixo da Diagonal Secundária.py
@@ -0,0 +1,16 @@
+caracter = str(input())
+soma = 0
+contador = 12
+
+for linha in range(0,12):
+    for coluna in range(0,12):
+        n = float(input())
+        if (contador <= coluna):
+            soma += n
+    contador -= 1
+
+if (caracter == 'S'):
+    print(f'{soma:.1f}')
+
+elif (caracter == 'M'):
+    print(f'{soma/66:.1f}')
