c44820a24faf7cd6863b07607e1c01529e9b0305
Gustavo Eguchi <gustavoeguchi10@gmail.com>
2025-02-26 18:54:27 -0300

    1435 - IA

--- Matriz Quadrada I.py
+++ Matriz Quadrada I.py
@@ -1,20 +1,30 @@
-while True:
-    num = int(input())
-    contador = 1
+def construir_matriz(n):
+    # Inicializa a matriz com zeros
+    matriz = [[0] * n for _ in range(n)]
+    
+    # Preenche a matriz com os valores apropriados
+    for camada in range((n + 1) // 2):  
+        for i in range(camada, n - camada):
+            matriz[camada][i] = camada + 1  # Preenche a linha superior
+            matriz[n - camada - 1][i] = camada + 1  # Preenche a linha inferior
+        for j in range(camada, n - camada):
+            matriz[j][camada] = camada + 1  # Preenche a coluna esquerda
+            matriz[j][n - camada - 1] = camada + 1  # Preenche a coluna direita
+    
+    return matriz
 
-    for linha in range(0,num):
-        for coluna in range(0,num):
-            if (coluna != num-1):
-                if (linha == 0 or coluna == 0 or linha == num-1):
-                    print(f'{contador}',end=' ')
-                else:
-                    if (num%2!=0 and coluna == num//2 and coluna == linha):
-                        print(f'{contador+(num//2)}',end=' ')
-                    else:
-                        if (coluna == linha or coluna * linha == num//2):
-                            print(f'{num//2}',end=' ')
-            else:
-                print(f'{contador}')
-    print('\n')
-    if (num == 0):
-        break
+def imprimir_matriz(matriz):
+    for linha in matriz:
+        print(" ".join(f"{valor:3}" for valor in linha))
+
+def main():
+    while True:
+        n = int(input())
+        if n == 0:
+            break
+        matriz = construir_matriz(n)
+        imprimir_matriz(matriz)
+        print()  # Linha em branco após cada matriz
+
+if __name__ == "__main__":
+    main()
