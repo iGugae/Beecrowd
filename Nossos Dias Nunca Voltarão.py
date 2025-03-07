13c6a9aa8e72156f18288f26452236a4cfff0ffe
Gustavo Eguchi <gustavoeguchi10@gmail.com>
2025-03-07 20:14:29 -0300

    1864

---
+++ Nossos Dias Nunca Voltarão.py
@@ -0,0 +1,7 @@
+frase = ['L','I','F','E',' ','I','S',' ','N','O','T',' ','A',' ','P','R','O','B','L','E','M',' ','T','O',' ','B','E',' ','S','O','L','V','E','D']
+
+num = int(input())
+for i in range(0,34):
+    if (i < num):
+        print(frase[i],end='')
+print()
