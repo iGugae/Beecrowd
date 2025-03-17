b096fc467c703f1a319557a09a11a2f5f55aacce
Gustavo Eguchi <gustavoeguchi10@gmail.com>
2025-03-17 17:30:02 -0300

    1973

--- Jornada nas Estrelas.py
+++ Jornada nas Estrelas.py
@@ -1,21 +1,23 @@
-num = int(input())
-lista = list(map(int, input().split()))
+def main():
+    from sys import stdin
+    num = int(stdin.readline())
+    lista = list(map(int, stdin.readline().split()))
 
-sitios = 0
-posicao = 0
+    sitios = 0
+    posicao = 0
 
-while (posicao >= 0 and posicao < num):
-    if (lista[posicao] > 0):
-        if posicao == 0:
-            sitios = posicao
-        if posicao > sitios:
-            sitios = posicao
-        lista[posicao] -= 1
-        if ((lista[posicao]+1) %2 == 0):
-            posicao -= 1
+    while (posicao >= 0 and posicao < num):
+        if (lista[posicao] > 0):
+            if posicao > sitios:
+                sitios = posicao
+            lista[posicao] -= 1
+            if ((lista[posicao]+1) %2 == 0):
+                posicao -= 1
+            else:
+                posicao += 1
         else:
-            posicao += 1
-    else:
-        break
+            break
+    print((sitios+1), sum(lista))
 
-print(sitios+1, sum(lista))
+if __name__ == "__main__":
+    main()
